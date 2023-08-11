408c6ff76f28b5bda58800912512f76f6c558245
dukuaris <dukuaris@gmail.com>
2023-08-11 16:56:37 +0900

    Building ThreadCard 03:13:43

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -33,3 +33,34 @@ export async function createThread({text, author, communityId, path }: Params) {
   }
   
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB()
+
+  // Calculate the number of posts to skip
+  const skipAmount = (pageNumber -1) * pageSize
+
+  // Fetch the posts that have no parents (top-level threads...)
+  const postQuery = Thread.find({
+    parentId: { $in: [null, undefined]}
+  })
+  .sort({ createdAt:'desc' })
+  .skip(skipAmount)
+  .limit(pageSize)
+  .populate({path: 'author', model: User})
+  .populate({
+    path: 'children',
+    model: User,
+    select: "_id name parentId image",
+  })
+
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined]}
+  })
+
+  const posts = await postQuery.exec()
+
+  const isNext = totalPostsCount > skipAmount + posts.length 
+
+  return { posts, isNext}
+}
